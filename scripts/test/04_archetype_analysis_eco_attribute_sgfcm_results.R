b7d857e0baee196144b222fbed531efadc459986
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-10-15 16:46:28 -0600

    updated rasters used. clean up code and update parameters

--- scripts/test/04_archetype_analysis_eco_attribute_sgfcm_results.R
+++ scripts/test/04_archetype_analysis_eco_attribute_sgfcm_results.R
@@ -9,19 +9,9 @@ library(viridis)
 
 
 #----Load the data----
-#ref_rast <- rast("/Users/katiemurenbeeld/Analysis/Archetype_Analysis/data/processed/rast_stack_all_attributes_2024-10-01.tif")
-rst_sc <- rast("/Users/katiemurenbeeld/Analysis/Archetype_Analysis/data/processed/rast_stack_eco_attributes_scaled_2024-10-03.tif")
-rst <- rast("/Users/katiemurenbeeld/Analysis/Archetype_Analysis/data/processed/rast_stack_eco_attributes_2024-10-03.tif")
+rst_sc <- rast("/Users/katiemurenbeeld/Analysis/Archetype_Analysis/data/processed/rast_stack_eco_attributes_scaled_2024-10-08.tif")
+rst <- rast("/Users/katiemurenbeeld/Analysis/Archetype_Analysis/data/processed/rast_stack_eco_attributes_2024-10-08.tif")
 
-
-#rst_resamp <- resample(rst_sc, ref_rast)
-
-# Format for use in geocmeans
-#dataset <- lapply(names(rst_resamp), function(n){
-#  aband <- rst_resamp[[n]]
-#  return(aband)
-#})
-#names(dataset) <- names(rst_resamp)
 # Format for use in geocmeans
 dataset <- lapply(names(rst_sc), function(n){
   aband <- rst_sc[[n]]
@@ -33,24 +23,19 @@ names(dataset) <- names(rst_sc)
 
 
 # Use Spatial Generalized Fuzzy C-Means clustering 
-# Highest silhouette index = 0.48 with k = 8, m = 1.8, beta = 0.3, alpha = 1.3
+# Highest silhouette index = 0.54 with k = 3, m = 1.6, beta = 0.4, alpha = 0.7
 # and a 3x3 window
 
-w1 <- matrix(1, nrow = 3, ncol = 3)
+w <- matrix(1, nrow = 3, ncol = 3)
 
-SGFCM_eco_result <- SGFCMeans(dataset, k = 3, m = 1.7, standardize = FALSE,
+SGFCM_eco_result <- SGFCMeans(dataset, k = 3, m = 1.6, standardize = FALSE,
                               lag_method = "mean",
-                              window = w1, alpha = 0.6, beta = 0.3,
+                              window = w, alpha = 0.8, beta = 0.1,
                               seed = 6891, tol = 0.001, verbose = TRUE, init = "kpp")
 
 map_SGFCM_result <- rast(SGFCM_eco_result$rasters)
 plot(map_SGFCM_result[["Groups"]])
 writeRaster(map_SGFCM_result[["Groups"]], filename = paste0("/Users/katiemurenbeeld/Analysis/Archetype_Analysis/outputs/SGFCM_eco_result_", 
-                                                            Sys.Date(), ".tif"))
+                                                            Sys.Date(), ".tif"), overwrite = TRUE)
 
-
-SGFCM_eco_result_k8 <- SGFCMeans(dataset, k = 8, m = 1.7, standardize = FALSE,
-                              lag_method = "mean",
-                              window = w1, alpha = 0.6, beta = 0.3,
-                              seed = 6891, tol = 0.001, verbose = TRUE, init = "kpp")
 

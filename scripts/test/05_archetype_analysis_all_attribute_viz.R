b1506dbfac1848017170da2ed32d106f68a40345
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-10-15 16:45:14 -0600

    added violin plots

--- scripts/test/05_archetype_analysis_all_attribute_viz.R
+++ scripts/test/05_archetype_analysis_all_attribute_viz.R
@@ -14,9 +14,13 @@ library(tigris)
 # Load the data
 fs_nf <- st_read("/Users/katiemurenbeeld/Analysis/Archetype_Analysis/data/original/S_USA.AdministrativeForest.shp")
 fs_reg <- st_read("/Users/katiemurenbeeld/Analysis/Archetype_Analysis/data/original/S_USA.AdministrativeRegion.shp")
-sgfcm_all_attri <- rast("/Users/katiemurenbeeld/Analysis/Archetype_Analysis/data/processed/rast_stack_all_attributes_2024-10-01.tif")
-sgfcm_all_result <- rast("/Users/katiemurenbeeld/Analysis/Archetype_Analysis/outputs/SGFCM_all_result_2024-10-02.tif") 
+sgfcm_all_attri <- rast("/Users/katiemurenbeeld/Analysis/Archetype_Analysis/data/processed/rast_stack_all_attributes_2024-10-08.tif")
+sgfcm_all_result <- rast("/Users/katiemurenbeeld/Analysis/Archetype_Analysis/outputs/SGFCM_all_result_2024-10-09.tif")
 plot(sgfcm_all_result)
+
+sgfcm_all_result_k6_mod <- readRDS(here::here("outputs/SGFCM_all_attr_k6_2024-10-15.rds"))
+sgfcm_all_result_k8_mod <- readRDS(here::here("outputs/SGFCM_all_attr_k8_2024-10-15.rds"))
+
 ## Reproject the forest service shapes to NAD83
 projection <- "epsg: 5070"
 
@@ -53,3 +57,63 @@ ggsave(paste0("~/Analysis/Archetype_Analysis/figures/sgfcm_all_reg_nf_map_", Sys
        plot = all_rg_nf_map, width = 12, height = 12, dpi = 300)  
 
 
+# Create violin plots
+df_all <- as.data.frame(scale(rst), na.rm = TRUE)
+vplots_k6 <- violinPlots(df_all, sgfcm_all_result_k6_mod$Groups)
+vplots_k8 <- violinPlots(df_all, sgfcm_all_result_k8_mod$Groups)
+
+# for k = 6
+for ( i in 1:21){
+  print(i)
+  vplots_tmp <- vplots_k6[[i]] +
+    scale_x_discrete(labels=c("A 1", "A 2", 
+                              "A 3", "A 4",
+                              "A 5", "A 6")) + 
+    scale_fill_brewer(labels=c("A 1", "A 2", 
+                               "A 3", "A 4",
+                               "A 5", "A 6"),
+                      palette = "Set2") +
+    geom_violin() + 
+    geom_hline(yintercept = 0) +
+    labs(title = "SGFCM k = 6", 
+         subtitle = names(df_all)[i],
+         fill = "Archetype") +
+    theme_bw() + 
+    theme(text = element_text(size = 20),
+          axis.title.x = element_blank(),
+          axis.text.x = element_text(size = 16), 
+          axis.title.y = element_blank(),
+          axis.text.y = element_text(size = 16))
+  plot(vplots_tmp)
+  ggsave(paste0("~/Analysis/SES_Forest_Archetypes/outputs/plots/SGFCM_k6_vplot_", names(df_all)[i], "_", Sys.Date(), ".png"), 
+         plot = vplots_tmp, width = 12, height = 12, dpi = 300)
+}
+
+# for k = 8
+for ( i in 1:21){
+  print(i)
+  vplots_tmp <- vplots_k8[[i]] +
+    scale_x_discrete(labels=c("A 1", "A 2", 
+                              "A 3", "A 4",
+                              "A 5", "A 6", 
+                              "A 7", "A 8")) + 
+    scale_fill_brewer(labels=c("A 1", "A 2", 
+                               "A 3", "A 4",
+                               "A 5", "A 6", 
+                               "A 7", "A 8"),
+                      palette = "Set2") +
+    geom_violin() + 
+    geom_hline(yintercept = 0) +
+    labs(title = "SGFCM k = 8", 
+         subtitle = names(df_all)[i],
+         fill = "Archetype") +
+    theme_bw() + 
+    theme(text = element_text(size = 20),
+          axis.title.x = element_blank(),
+          axis.text.x = element_text(size = 16), 
+          axis.title.y = element_blank(),
+          axis.text.y = element_text(size = 16))
+  #plot(vplots_tmp)
+  ggsave(paste0("~/Analysis/SES_Forest_Archetypes/outputs/plots/SGFCM_k8_vplot_", names(df_all)[i], "_", Sys.Date(), ".png"), 
+         plot = vplots_tmp, width = 12, height = 12, dpi = 300)
+}

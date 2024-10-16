b049ab4e0324a7d1eb8c353a7fb834d1f3f5bb9f
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-10-16 16:03:04 -0600

    updates to plots and save plots

--- scripts/test/06_archetype_analysis_all_attribute_var_interp.R
+++ scripts/test/06_archetype_analysis_all_attribute_var_interp.R
@@ -60,33 +60,87 @@ k8_sd_long <- data %>%
 k8_long <- left_join(k8_means_long, k8_sd_long)
 
 
-ggplot(k6_long, aes(x = var_name, y = mean, fill = groups_k6)) +
+k6_var_interp <- ggplot(k6_long, aes(x = var_name, y = mean, fill = groups_k6)) +
   geom_col() +
   geom_errorbar(aes(ymin=mean-sd, ymax=mean+sd), width=.2,
                 position=position_dodge(.9)) +
+  scale_fill_brewer(palette = "Set2") +
   coord_flip() +
-  facet_wrap(~groups_k6)
+  facet_wrap(~groups_k6) +
+  theme(text = element_text(size = 20),
+        legend.position = "none", 
+        axis.title.y = element_blank()) 
+
+
+ggsave(paste0("~/Analysis/Archetype_Analysis/figures/sgfcm_all_k6_var_interp_", Sys.Date(), ".png"), 
+       plot = k6_var_interp, width = 12, height = 8, dpi = 300) 
 
 ggplot(k8_long, aes(x = var_name, y = mean, fill = groups_k8)) +
   geom_col() +
   geom_errorbar(aes(ymin=mean-sd, ymax=mean+sd), width=.2,
                 position=position_dodge(.9)) +
   coord_flip() +
-  facet_wrap(~groups_k8, ncol = 4)
+  facet_wrap(~groups_k8, ncol = 4) +
+  theme(legend.position = "none", 
+        axis.title.y = element_blank()) 
 
+#----Create violin plots-------------
+df_all <- as.data.frame(scale(rst), na.rm = TRUE)
+vplots_k6 <- violinPlots(df_all, sgfcm_all_result_k6_mod$Groups)
+vplots_k8 <- violinPlots(df_all, sgfcm_all_result_k8_mod$Groups)
 
-
-data_summary <- function(data, varname, groupnames){
-  require(plyr)
-  summary_func <- function(x, col){
-    c(mean = mean(x[[col]], na.rm=TRUE),
-      sd = sd(x[[col]], na.rm=TRUE))
-  }
-  data_sum<-ddply(data, groupnames, .fun=summary_func,
-                  varname)
-  data_sum <- rename(data_sum, c("mean" = varname))
-  return(data_sum)
+# for k = 6
+for ( i in 1:21){
+  print(i)
+  vplots_tmp <- vplots_k6[[i]] +
+    scale_x_discrete(labels=c("A 1", "A 2", 
+                              "A 3", "A 4",
+                              "A 5", "A 6")) + 
+    scale_fill_brewer(labels=c("A 1", "A 2", 
+                               "A 3", "A 4",
+                               "A 5", "A 6"),
+                      palette = "Set2") +
+    geom_violin() + 
+    geom_hline(yintercept = 0) +
+    labs(title = "SGFCM k = 6", 
+         subtitle = names(df_all)[i],
+         fill = "Archetype") +
+    theme_bw() + 
+    theme(text = element_text(size = 20),
+          axis.title.x = element_blank(),
+          axis.text.x = element_text(size = 16), 
+          axis.title.y = element_blank(),
+          axis.text.y = element_text(size = 16))
+  plot(vplots_tmp)
+  ggsave(paste0("~/Analysis/SES_Forest_Archetypes/outputs/plots/SGFCM_k6_vplot_", names(df_all)[i], "_", Sys.Date(), ".png"), 
+         plot = vplots_tmp, width = 12, height = 12, dpi = 300)
 }
 
-df2 <- data_summary(data, varname=, 
-                    groupnames=c("groups_k6", "dose"))
+# for k = 8
+for ( i in 1:21){
+  print(i)
+  vplots_tmp <- vplots_k8[[i]] +
+    scale_x_discrete(labels=c("A 1", "A 2", 
+                              "A 3", "A 4",
+                              "A 5", "A 6", 
+                              "A 7", "A 8")) + 
+    scale_fill_brewer(labels=c("A 1", "A 2", 
+                               "A 3", "A 4",
+                               "A 5", "A 6", 
+                               "A 7", "A 8"),
+                      palette = "Set2") +
+    geom_violin() + 
+    geom_hline(yintercept = 0) +
+    labs(title = "SGFCM k = 8", 
+         subtitle = names(df_all)[i],
+         fill = "Archetype") +
+    theme_bw() + 
+    theme(text = element_text(size = 20),
+          axis.title.x = element_blank(),
+          axis.text.x = element_text(size = 16), 
+          axis.title.y = element_blank(),
+          axis.text.y = element_text(size = 16))
+  #plot(vplots_tmp)
+  ggsave(paste0("~/Analysis/SES_Forest_Archetypes/outputs/plots/SGFCM_k8_vplot_", names(df_all)[i], "_", Sys.Date(), ".png"), 
+         plot = vplots_tmp, width = 12, height = 12, dpi = 300)
+}

23401825c1c8d5c98539d3924408a943e34b8781
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2024-10-22 07:04:39 -0600

    updated comments with parameters and silhouette index scores

--- scripts/test/04_archetype_analysis_soc_attribute_sgfcm_results.R
+++ scripts/test/04_archetype_analysis_soc_attribute_sgfcm_results.R
@@ -23,22 +23,35 @@ names(dataset) <- names(rst_sc)
 
 
 # Use Spatial Generalized Fuzzy C-Means clustering 
-# Highest silhouette index = 0.48 with k = 8, m = 1.8, beta = 0.3, alpha = 1.3
-# and a 3x3 window
-# Silhouette index = 0.86, k = 3, m = 1.2, beta = 0.5, alpha = 0.9, window = 3x3
+# Silhouette index = 0.87, k = 3, m = 1.2, beta = 0.2, alpha = 0.7, window = 3x3
+# Silhouette index = 0.35, k = 6, m = 1.8, beta = 0.7, alpha = 1.4, window = 5x5
 
-w <- matrix(1, nrow = 3, ncol = 3)
+w1 <- matrix(1, nrow = 3, ncol = 3)
+w2 <- matrix(1, nrow = 5, ncol = 5)
 
-SGFCM_soc_result <- SGFCMeans(dataset, k = 8, m = 1.8, standardize = FALSE,
+SGFCM_soc_result_k3 <- SGFCMeans(dataset, k = 3, m = 1.2, standardize = FALSE,
                               lag_method = "mean",
-                              window = w, alpha = 1.3, beta = 0.3,
+                              window = w1, alpha = 0.7, beta = 0.2,
                               seed = 6891, tol = 0.001, verbose = TRUE, init = "kpp")
 
-map_SGFCM_result <- rast(SGFCM_soc_result$rasters)
-plot(map_SGFCM_result[["Groups"]])
-writeRaster(map_SGFCM_result[["Groups"]], filename = paste0("/Users/katiemurenbeeld/Analysis/Archetype_Analysis/outputs/SGFCM_soc_result_", 
+map_SGFCM_result_k3 <- rast(SGFCM_soc_result_k3$rasters)
+plot(map_SGFCM_result_k3[["Groups"]])
+writeRaster(map_SGFCM_result_k3[["Groups"]], filename = paste0("/Users/katiemurenbeeld/Analysis/Archetype_Analysis/outputs/SGFCM_soc_result_k3_", 
                                                             Sys.Date(), ".tif"), overwrite = TRUE)
+saveRDS(SGFCM_soc_result_k3, paste0("/Users/katiemurenbeeld/Analysis/SES_Forest_Archetypes/outputs/SGFCM_soc_attr_k3_", 
+                                 Sys.Date(), ".rds"))
 
+SGFCM_soc_result_k6 <- SGFCMeans(dataset, k = 6, m = 1.8, standardize = FALSE,
+                                 lag_method = "mean",
+                                 window = w2, alpha = 1.4, beta = 0.7,
+                                 seed = 6891, tol = 0.001, verbose = TRUE, init = "kpp")
+
+map_SGFCM_result_k6 <- rast(SGFCM_soc_result_k6$rasters)
+plot(map_SGFCM_result_k6[["Groups"]])
+writeRaster(map_SGFCM_result_k6[["Groups"]], filename = paste0("/Users/katiemurenbeeld/Analysis/Archetype_Analysis/outputs/SGFCM_soc_result_k6_", 
+                                                               Sys.Date(), ".tif"), overwrite = TRUE)
+saveRDS(SGFCM_soc_result_k6, paste0("/Users/katiemurenbeeld/Analysis/SES_Forest_Archetypes/outputs/SGFCM_soc_attr_k6_", 
+                                 Sys.Date(), ".rds"))
 
 ordered_eco_soc <- groups_matching(SGFCM_eco_result, SGFCM_soc_result)
 plot(ordered_eco_soc$rasters$group1)

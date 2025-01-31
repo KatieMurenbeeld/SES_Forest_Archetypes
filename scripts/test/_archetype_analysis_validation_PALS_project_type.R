a07b544b40d5377ce197e67df7a6d9d623decd32
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2025-01-31 13:08:57 -0700

    made more barplots with the ent-div and with the high-high in their own category

--- scripts/test/_archetype_analysis_validation_PALS_project_type.R
+++ scripts/test/_archetype_analysis_validation_PALS_project_type.R
@@ -82,7 +82,8 @@ pals_df <- read_delim("~/Analysis/NEPA_Efficiency/data/original/pals_ongoing_pro
 # Filter for date and select Forest Number and Purposes
 pals_df_2009 <- pals_df %>%
   filter(as.Date(`INITIATION DATE`, format = "%m/%d/%Y") >= "2009-01-01") %>%
-  dplyr::select(FOREST_ID, `FC Facility management – purpose`, 
+  filter(REGION_ID != "10" & REGION_ID != "13" & REGION_ID != "24" & REGION_ID != "00") %>%
+  dplyr::select(REGION_ID, FOREST_ID, `FC Facility management – purpose`, 
                 `FR Research – purpose`, `HF Fuels management – purpose`, `HR Heritage resource management – purpose`,
                 `LM Land ownership management – purpose`, `LW Land acquisition – purpose`,
                 `MG Minerals and geology – purpose`, `PN Land management planning – purpose`,
@@ -92,7 +93,8 @@ pals_df_2009 <- pals_df %>%
                 `VM Vegetation management (non-forest products) – purpose`,
                 `WF Wildlife, fish, rare plants – purpose`, `WM Water management – purpose`) %>%
   group_by(FOREST_ID) %>%
-  summarise(p_facilities = sum(`FC Facility management – purpose`), 
+  summarise(REGION = mean(as.numeric(REGION_ID)),
+            p_facilities = sum(`FC Facility management – purpose`), 
             p_research = sum(`FR Research – purpose`),
             p_haz_fuels = sum(`HF Fuels management – purpose`),
             p_heritage = sum(`HR Heritage resource management – purpose`),
@@ -156,8 +158,10 @@ pals_purpose_arch_pct_area <- left_join(pals_df_2009, areas_wide, by = join_by(F
 # Filter for date and summarise the elapsed days and NEPA type counts
 pals_df_2009_nepa_time <- pals_df %>%
   filter(as.Date(`INITIATION DATE`, format = "%m/%d/%Y") >= "2009-01-01") %>%
+  filter(REGION_ID != "10" & REGION_ID != "13" & REGION_ID != "24" & REGION_ID != "00") %>%
   group_by(FOREST_ID) %>%
-  summarise(mean_nepa_time = mean(`ELAPSED DAYS`, na.rm = TRUE),
+  summarise(REGION = mean(as.numeric(REGION_ID)),
+            mean_nepa_time = mean(`ELAPSED DAYS`, na.rm = TRUE),
             med_nepa_time = median(`ELAPSED DAYS`, na.rm = TRUE))
 
 pals_df_2009_nepa_time_year <- pals_df %>%
@@ -170,6 +174,7 @@ pals_df_2009_nepa_time_year <- pals_df %>%
 
 pals_df_2009_nepa_type <- pals_df %>%
   filter(as.Date(`INITIATION DATE`, format = "%m/%d/%Y") >= "2009-01-01") %>%
+  filter(REGION_ID != "10" & REGION_ID != "13" & REGION_ID != "24" & REGION_ID != "00") %>%
   group_by(FOREST_ID) %>%
   count(`DECISION TYPE`) %>%
   pivot_wider(names_from = `DECISION TYPE`, values_from = n, values_fill = 0) 
@@ -193,8 +198,8 @@ nf_arch_year_df <- left_join(pals_df_2009_nepa_time_year, pals_df_2009_nepa_type
 nf_arch_year_df_test <- right_join(nf_arch_year_df, nf_summ_df, by = c("FOREST_ID" = "forest_num"))
 
 # save the csv file
-write_csv(nf_arch_summ_df, here::here(paste0("outputs/tables/nf_level_dominant_archetypes_uncertainty_nepa_", Sys.Date(), ".csv")))
-write_csv(nf_arch_year_df_test, here::here(paste0("outputs/tables/nf_level_pals_year_arch_summs_", Sys.Date(), ".csv")))
+#write_csv(nf_arch_summ_df, here::here(paste0("outputs/tables/nf_level_dominant_archetypes_uncertainty_nepa_", Sys.Date(), ".csv")))
+#write_csv(nf_arch_year_df_test, here::here(paste0("outputs/tables/nf_level_pals_year_arch_summs_", Sys.Date(), ".csv")))
 
 # look at region 4 
 pals_arch_reg4 <- pals_purpose_arch_pct_area %>%
@@ -314,7 +319,7 @@ ggsave(here::here(paste0("outputs/plots/regs_proj_purpose_by_forest_",
 # could filter by forests with >70% in any specific archetype
 
 # try with archetypes 1 and 4 first
-dom_pct <- 70
+dom_pct <- 50
 
 arche1 <-  pals_purpose_arch_pct_area %>%
   filter(`1` >= dom_pct) %>%
@@ -407,6 +412,209 @@ ggsave(filename = here::here(paste0("outputs/plots/archetype_validation_test_70_
        width = 12, 
        height = 4, dpi = 300)
 
+#---Without looking at dominant archetype or by filtering out high ent - high div forests
+
+nf_pals_df <- left_join(nf_arch_summ_df, pals_df_2009, by = "FOREST_ID")
+
+nf_pal_lowlow <- nf_pals_df %>%
+  filter(div_to_ent == "low_ent_low_div") %>%
+  select(FOREST_ID, REGION.x, starts_with("p_"))
+
+nf_pal_lowhigh <- nf_pals_df %>%
+  filter(div_to_ent == "low_ent_high_div") %>%
+  select(FOREST_ID, REGION.x, starts_with("p_"))
+
+nf_pal_highlow <- nf_pals_df %>%
+  filter(div_to_ent == "high_ent_low_div") %>%
+  select(FOREST_ID, REGION.x, starts_with("p_"))
+
+nf_pal_highhigh <- nf_pals_df %>%
+  filter(div_to_ent == "high_ent_high_div") %>%
+  select(FOREST_ID, REGION.x, starts_with("p_"))
+
+lowlow <- nf_pal_lowlow %>%
+  pivot_longer(cols = starts_with("p_"), 
+               names_to = "purpose") %>%
+  group_by(purpose) %>%
+  summarise(values = sum(value, na.rm = TRUE)) %>%
+  mutate(pct_purpose = values/sum(values) * 100,
+         ent_div = "low-low")
+
+lowhigh <- nf_pal_lowhigh %>%
+  pivot_longer(cols = starts_with("p_"), 
+               names_to = "purpose") %>%
+  group_by(purpose) %>%
+  summarise(values = sum(value, na.rm = TRUE)) %>%
+  mutate(pct_purpose = values/sum(values) * 100,
+         ent_div = "low-high")
+
+highlow <- nf_pal_highlow %>%
+  pivot_longer(cols = starts_with("p_"), 
+               names_to = "purpose") %>%
+  group_by(purpose) %>%
+  summarise(values = sum(value, na.rm = TRUE)) %>%
+  mutate(pct_purpose = values/sum(values) * 100,
+         ent_div = "high-low")
+
+highhigh <- nf_pal_highhigh %>%
+  pivot_longer(cols = starts_with("p_"), 
+               names_to = "purpose") %>%
+  group_by(purpose) %>%
+  summarise(values = sum(value)) %>%
+  mutate(pct_purpose = values/sum(values) * 100,
+         ent_div = "high-high")
+
+ent_div_purpose <- rbind(lowlow, lowhigh, highlow, highhigh)
+
+ent_div_purpose_plot <- ggplot(ent_div_purpose, aes(x=purpose, y = pct_purpose, fill = ent_div)) +
+  geom_bar(stat = "identity", width = 0.7, position = position_dodge(), color = "black") +
+  scale_fill_discrete(limits=c("low-low", "low-high", "high-low", "high-high")) + 
+  scale_fill_met_d("Hokusai3") +
+  theme_minimal() + 
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1))
+ent_div_purpose_plot
+ggsave(filename = here::here(paste0("outputs/plots/archetype_validation_test_ent_div_purpose_", Sys.Date(), ".png")), 
+       plot = ent_div_purpose_plot, 
+       width = 12, 
+       height = 4, dpi = 300)
+
+nf_pal_nohighhigh_domarch <- nf_pals_df %>%
+  filter(div_to_ent != "high_ent_high_div") %>%
+  select(FOREST_ID, REGION.x, starts_with("p_"), dom_archetype)
+
+nf_pal_highhigh_domarch <- nf_pals_df %>%
+  filter(div_to_ent == "high_ent_high_div") %>%
+  select(FOREST_ID, REGION.x, starts_with("p_"), dom_archetype)
+
+
+a1_nhh <-  nf_pal_nohighhigh_domarch %>%
+  filter(dom_archetype == 1) %>%
+  pivot_longer(cols = starts_with("p_"), 
+               names_to = "purpose") %>%
+  group_by(purpose) %>%
+  summarise(values = sum(value)) %>%
+  mutate(archetype = "A", 
+         pct_purpose = values/sum(values) * 100)
+
+a2_nhh <-  nf_pal_nohighhigh_domarch %>%
+  filter(dom_archetype == 2) %>%
+  pivot_longer(cols = starts_with("p_"), 
+               names_to = "purpose") %>%
+  group_by(purpose) %>%
+  summarise(values = sum(value, na.rm = TRUE)) %>%
+  mutate(archetype = "B", 
+         pct_purpose = values/sum(values) * 100)
+
+a3_nhh <-  nf_pal_nohighhigh_domarch %>%
+  filter(dom_archetype == 3) %>%
+  pivot_longer(cols = starts_with("p_"), 
+               names_to = "purpose") %>%
+  group_by(purpose) %>%
+  summarise(values = sum(value)) %>%
+  mutate(archetype = "C", 
+         pct_purpose = values/sum(values) * 100)
+
+a4_nhh <-  nf_pal_nohighhigh_domarch %>%
+  filter(dom_archetype == 4) %>%
+  pivot_longer(cols = starts_with("p_"), 
+               names_to = "purpose") %>%
+  group_by(purpose) %>%
+  summarise(values = sum(value)) %>%
+  mutate(archetype = "D", 
+         pct_purpose = values/sum(values) * 100)
+
+a5_nhh <-  nf_pal_nohighhigh_domarch %>%
+  filter(dom_archetype == 5) %>%
+  pivot_longer(cols = starts_with("p_"), 
+               names_to = "purpose") %>%
+  group_by(purpose) %>%
+  summarise(values = sum(value)) %>%
+  mutate(archetype = "E", 
+         pct_purpose = values/sum(values) * 100)
+
+a6_nhh <-  nf_pal_nohighhigh_domarch %>%
+  filter(dom_archetype == 6) %>%
+  pivot_longer(cols = starts_with("p_"), 
+               names_to = "purpose") %>%
+  group_by(purpose) %>%
+  summarise(values = sum(value)) %>%
+  mutate(archetype = "F", 
+         pct_purpose = values/sum(values) * 100)
+
+hh <- nf_pal_highhigh_domarch %>%
+  pivot_longer(cols = starts_with("p_"), 
+               names_to = "purpose") %>%
+  group_by(purpose) %>%
+  summarise(values = sum(value)) %>%
+  mutate(archetype = "high_high", 
+         pct_purpose = values/sum(values) * 100)
+
+test_dom_hh <- rbind(a1_nhh, a2_nhh, a3_nhh, a4_nhh, a5_nhh, a6_nhh, hh)
+
+test_dom_hh_plot <- ggplot(test_dom_hh, aes(x=purpose, y = pct_purpose, fill = archetype)) +
+  geom_bar(stat = "identity", width = 0.7, position = position_dodge(), color = "black") +
+  scale_fill_discrete(limits=c("A", "B", "C", "D", "E", "F", "high ent - high div")) + 
+  scale_fill_met_d("Hokusai3") +
+  theme_minimal() + 
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1))
+test_dom_hh_plot
+ggsave(filename = here::here(paste0("outputs/plots/archetype_validation_test_nothres_highhigh_purpose_", Sys.Date(), ".png")), 
+       plot = test_dom_hh_plot, 
+       width = 12, 
+       height = 4, dpi = 300)
+
+
+# I still want to figure something out with the regions and archetypes? 
+nf_pals_year_df <- left_join(nf_arch_year_df_test, pals_df_2009, by = "FOREST_ID")
+
+r1 <- nf_pals_year_df %>%
+  filter(REGION.x == 1)
+r2 <- nf_pals_year_df %>%
+  filter(REGION.x == 2)
+r3 <- nf_pals_year_df %>%
+  filter(REGION.x == 3)
+r4 <- nf_pals_year_df %>%
+  filter(REGION.x == 4)
+r5 <- nf_pals_year_df %>%
+  filter(REGION.x == 5)
+r6 <- nf_pals_year_df %>%
+  filter(REGION.x == 6)
+r8 <- nf_pals_year_df %>%
+  filter(REGION.x == 8)
+r9 <- nf_pals_year_df %>%
+  filter(REGION.x == 9)
+
+r4_filt <- pals_df %>%
+  filter(as.Date(`INITIATION DATE`, format = "%m/%d/%Y") >= "2009-01-01") %>%
+  filter(FOREST_ID %in% c("0402", "0412", "0407", "0408", "0410")) %>%
+  filter(`ELAPSED DAYS` >= 0) %>%
+  mutate(forname_fct = factor(`LMU – FOREST`, levels = c("BoNF",
+                                                         "PaNF",
+                                                         "DiNF",
+                                                         "FiNF",
+                                                         "MLNF")))
+
+r4_filt_test <- nf_pals_year_df %>%
+  filter(FOREST_ID %in% c("0402", "0412", "0407", "0408", "0410")) %>%
+  mutate(forname_fct = factor(forest_name, levels = c("Boise National Forest", 
+                                                      "Payette National Forest",
+                                                      "Dixie National Forest", 
+                                                      "Fishlake National Forest",
+                                                      "Manti-La Sal National Forest")))
+  #filter(`ELAPSED DAYS` >= 0) %>%
+  #mutate(forname_fct = factor(`LMU – FOREST`, levels = c("BoNF",
+                                                         
+
+r4_filt_boxplots <- ggplot(r4_filt_test, aes(x = forname_fct, y = ((DN + ROD) / med_nepa_time) , fill = forname_fct)) + 
+  geom_boxplot() 
+  #facet_wrap(~`SIGNED FY`)
+r4_filt_boxplots
+ggsave(filename = here::here(paste0("outputs/plots/archetype_validation_r4_forests_boxplots_med_time_", Sys.Date(), ".png")), 
+       plot = r4_filt_boxplots, 
+       width = 12, 
+       height = 6, dpi = 300)
 
 #----What does this mean for NEPA assessment times?----
 # selected forests from PMRC draft

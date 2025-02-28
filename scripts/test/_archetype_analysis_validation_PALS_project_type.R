5e0bf894372beccc6af139f2fef9753260aa6412
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2025-02-28 11:26:22 -0700

    added code to try out a split violin plot

--- scripts/test/_archetype_analysis_validation_PALS_project_type.R
+++ scripts/test/_archetype_analysis_validation_PALS_project_type.R
@@ -256,8 +256,8 @@ nf_year_summ <- nf_arch_year_df %>%
             total_pct_EA_EIS = (total_EIS + total_EA)/(total_EIS + total_EA + total_CE) * 100)
 
 nf_year_summ_arch <- left_join(nf_summ_df, nf_year_summ, by = c("forest_num" = "FOREST_ID"))
-write_csv(nf_year_summ_arch, here::here(paste0("outputs/tables/nf_nepa_projs_arch_summ_", 
-                                               Sys.Date(), ".csv")))
+#write_csv(nf_year_summ_arch, here::here(paste0("outputs/tables/nf_nepa_projs_arch_summ_", 
+#                                               Sys.Date(), ".csv")))
 
 nf_year_summ_arch %>%
   filter(div_to_ent == "high_ent_high_div" | div_to_ent == "low_ent_low_div") %>%
@@ -392,6 +392,58 @@ nf_year_summ_arch %>%
 
 nf_year_summ_arch %>%
   filter(div_to_ent == "high_ent_high_div" | div_to_ent == "low_ent_low_div") %>%
+  ggplot(aes(x=as.factor(dom_archetype), y=yearly_med_projs, fill=div_to_ent)) + 
+  geom_boxplot()
+
+#----Split violin plto code from stackoverflow----
+GeomSplitViolin <- ggproto("GeomSplitViolin", GeomViolin, 
+                           draw_group = function(self, data, ..., draw_quantiles = NULL) {
+                             data <- transform(data, xminv = x - violinwidth * (x - xmin), xmaxv = x + violinwidth * (xmax - x))
+                             grp <- data[1, "group"]
+                             newdata <- plyr::arrange(transform(data, x = if (grp %% 2 == 1) xminv else xmaxv), if (grp %% 2 == 1) y else -y)
+                             newdata <- rbind(newdata[1, ], newdata, newdata[nrow(newdata), ], newdata[1, ])
+                             newdata[c(1, nrow(newdata) - 1, nrow(newdata)), "x"] <- round(newdata[1, "x"])
+                             
+                             if (length(draw_quantiles) > 0 & !scales::zero_range(range(data$y))) {
+                               stopifnot(all(draw_quantiles >= 0), all(draw_quantiles <=
+                                                                         1))
+                               quantiles <- ggplot2:::create_quantile_segment_frame(data, draw_quantiles)
+                               aesthetics <- data[rep(1, nrow(quantiles)), setdiff(names(data), c("x", "y")), drop = FALSE]
+                               aesthetics$alpha <- rep(1, nrow(quantiles))
+                               both <- cbind(quantiles, aesthetics)
+                               quantile_grob <- GeomPath$draw_panel(both, ...)
+                               ggplot2:::ggname("geom_split_violin", grid::grobTree(GeomPolygon$draw_panel(newdata, ...), quantile_grob))
+                             }
+                             else {
+                               ggplot2:::ggname("geom_split_violin", GeomPolygon$draw_panel(newdata, ...))
+                             }
+                           })
+
+geom_split_violin <- function(mapping = NULL, data = NULL, stat = "ydensity", position = "identity", ..., 
+                              draw_quantiles = NULL, trim = TRUE, scale = "area", na.rm = FALSE, 
+                              show.legend = NA, inherit.aes = TRUE) {
+  layer(data = data, mapping = mapping, stat = stat, geom = GeomSplitViolin, 
+        position = position, show.legend = show.legend, inherit.aes = inherit.aes, 
+        params = list(trim = trim, scale = scale, draw_quantiles = draw_quantiles, na.rm = na.rm, ...))
+}
+nf_year_summ_arch %>%
+  filter(div_to_ent == "high_ent_high_div" | div_to_ent == "low_ent_low_div") %>%
+  ggplot(aes(x=as.factor(dom_archetype), y=yearly_pct_med_EA_EIS, fill=div_to_ent)) + 
+  geom_split_violin()
+
+nf_year_summ_arch %>%
+  filter(div_to_ent == "high_ent_high_div" | div_to_ent == "low_ent_low_div") %>%
+  ggplot(aes(x=region, y=yearly_pct_med_EA_EIS, fill=div_to_ent)) + 
+  geom_split_violin()
+
+nf_year_summ_arch %>%
+  filter(div_to_ent == "high_ent_high_div" | div_to_ent == "low_ent_low_div") %>%
+  ggplot(aes(x=1, y=yearly_pct_med_EA_EIS, fill=div_to_ent)) + 
+  geom_split_violin()
+
+#----end split violine plot----
+nf_year_summ_arch %>%
+  filter(div_to_ent == "high_ent_high_div" | div_to_ent == "low_ent_low_div") %>%
   ggplot(aes(x=div_to_ent, y=yearly_pct_med_EA_EIS, fill=div_to_ent)) + 
   geom_boxplot()
 
@@ -433,7 +485,27 @@ nf_arch_summ_df %>%
 nf_arch_summ_df %>%
   filter(div_to_ent == "high_ent_high_div" | div_to_ent == "low_ent_low_div") %>%
   ggplot(aes(x=region, y=(ROD+DN)/(ROD+DN+DM)*100, fill = div_to_ent)) + 
-  geom_boxplot() 
+  geom_boxplot()
+
+nf_arch_summ_df %>%
+  #filter(div_to_ent == "high_ent_high_div" | div_to_ent == "low_ent_low_div") %>%
+  ggplot(aes(x=region, y=shan_diverse)) + 
+  geom_boxplot()
+
+nf_arch_summ_df %>%
+  #filter(div_to_ent == "high_ent_high_div" | div_to_ent == "low_ent_low_div") %>%
+  ggplot(aes(x=region, y=entropy_all)) + 
+  geom_boxplot()
+
+nf_arch_summ_df %>%
+  #filter(div_to_ent == "high_ent_high_div" | div_to_ent == "low_ent_low_div") %>%
+  ggplot(aes(x=as.factor(dom_archetype), y=shan_diverse)) + 
+  geom_boxplot()
+
+nf_arch_summ_df %>%
+  #filter(div_to_ent == "high_ent_high_div" | div_to_ent == "low_ent_low_div") %>%
+  ggplot(aes(x=as.factor(dom_archetype), y=entropy_all)) + 
+  geom_boxplot()
 
 # combine for select forests from Region 4
 

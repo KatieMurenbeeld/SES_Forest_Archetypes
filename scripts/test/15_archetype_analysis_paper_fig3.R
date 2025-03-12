badb455ef41b40fd643915a658a0c15845653e36
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2025-03-12 13:20:59 -0600

    update and clean up code for ggplot

--- scripts/test/15_archetype_analysis_paper_fig3.R
+++ scripts/test/15_archetype_analysis_paper_fig3.R
@@ -28,20 +28,18 @@ dom_arch_df <- dom_arch_df %>%
 div_ent <- dom_arch_df %>%
   ggplot() +
   geom_point(aes(x=scale(entropy_all), y=scale(shan_diverse), 
-                 color=as.factor(dom_arch_alpha), size = pct_area_dom_arch, 
-                 alpha = 0.25), show.legend = TRUE) + 
+                 color=as.factor(dom_arch_alpha), fill=as.factor(dom_arch_alpha), 
+                 size = pct_area_dom_arch, alpha = 0.25), show.legend = TRUE) + 
   geom_hline(yintercept = 0) + 
   geom_vline(xintercept = 0) +
-  #scale_color_brewer(palette = "Set2") + 
   scale_color_met_d("Hokusai3") +
-  #scale_x_continuous(breaks = c(-3,-2,-1,0,1,2,3), labels = axLabels) +
-  #scale_y_continuous(breaks = c(-3,-2,-1,0,1,2,3), labels = axLabels) +
   xlim(-3, 3) +
   ylim(-3, 3) +
   scale_size_continuous(name = "% Area Dom. Archetype") + 
   theme_bw() +
   guides(alpha = "none") +
   guides(size = "none") +
+  guides(fill = "none") +
   annotate("text", x = 2, y = 2, label= "high-high") + 
   annotate("text", x = -2, y = -3, label = "low-low") +
   annotate("text", x = -1.25, y = 2, label= "high-low") + 
@@ -55,4 +53,4 @@ div_ent <- dom_arch_df %>%
 div_ent
 
 ggsave(here::here(paste0("outputs/plots/diverse_entropy_sc_scatter_", Sys.Date(), ".png")),
-       div_ent, height = 4, width = 4, dpi = 300)
+       div_ent, height = 6, width = 6, dpi = 300)

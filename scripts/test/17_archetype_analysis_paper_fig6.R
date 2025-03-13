7951c14b2116333056aa031b64381d3de3ab735e
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2025-03-13 13:27:47 -0600

    script to create figure 6 in manuscript. Density plots of yearly median % EIS + EA and total EIS for the high-high and low-low (entropy-diversity) forests.

---
+++ scripts/test/17_archetype_analysis_paper_fig6.R
@@ -0,0 +1,56 @@
+library(tidyverse)
+library(ggplot2)
+library(MetBrewer)
+library(cowplot)
+library(patchwork)
+
+# load the data
+
+nf_year_summ_arch <- read_csv(here::here("outputs/tables/nf_nepa_projs_arch_summ_2025-02-21.csv"))
+
+# correlations
+cor(nf_year_summ_arch$shan_diverse, nf_year_summ_arch$yearly_pct_med_EA_EIS, use = "complete.obs")
+cor(nf_year_summ_arch$entropy_all, nf_year_summ_arch$yearly_pct_med_EA_EIS, use = "complete.obs")
+cor(nf_year_summ_arch$shan_diverse, nf_year_summ_arch$total_EIS, use = "complete.obs")
+cor(nf_year_summ_arch$entropy_all, nf_year_summ_arch$total_EIS, use = "complete.obs")
+
+# plot
+p1 <- nf_year_summ_arch %>%
+  filter(div_to_ent == "high_ent_high_div" | div_to_ent == "low_ent_low_div") %>%
+  ggplot(aes(x = yearly_pct_med_EA_EIS)) +
+  geom_histogram(aes(y = ..density.., fill = div_to_ent, alpha = 0.6), bins = 10) +
+  geom_density(aes(color = div_to_ent, alpha = 0.6)) +
+  xlab("Yearly Median % EIS + EA") + 
+  ylab("Density") +
+  scale_fill_manual(labels = c("High - High", "Low - Low"), values=met.brewer("Hokusai1", 2)) +
+  scale_color_manual(labels = c("High - High", "Low - Low"),values=met.brewer("Hokusai1", 2)) +
+  guides(fill = "none",
+         alpha = "none", 
+         color = "none") +
+  theme_minimal() + 
+  theme_bw()
+p1
+
+p2 <- nf_year_summ_arch %>%
+  filter(div_to_ent == "high_ent_high_div" | div_to_ent == "low_ent_low_div") %>%
+  ggplot(aes(x = total_EIS)) +
+  geom_histogram(aes(y = ..density.., fill = div_to_ent, alpha = 0.6), bins = 10) +
+  geom_density(aes(color = div_to_ent, alpha = 0.6)) +
+  xlab("Total EIS") + 
+  ylab("") +
+  scale_fill_manual(labels = c("High - High", "Low - Low"), values=met.brewer("Hokusai1", 2)) +
+  scale_color_manual(labels = c("High - High", "Low - Low"),values=met.brewer("Hokusai1", 2)) +
+  guides(fill = guide_legend(title = "Diversity - Entropy"),
+         alpha = "none", 
+         color = "none") +
+  theme_minimal() + 
+  theme_bw()
+p2
+
+panel <- p1 | p2 
+  #plot_layout(heights = unit(c(10, 1), c('cm', 'null')))
+panel
+
+# save
+ggsave(here::here(paste0("outputs/plots/archetype_analysis_fig6_testing_", Sys.Date(), ".png")), 
+       plot = panel, width = 12, height = 6, dpi = 300)

c3f2cbcde3ceec69d8d695bb79dbc0c123c37851
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2025-04-24 23:45:15 -0600

    added comments updated csv for table in appendix

--- scripts/test/_dom_archetype_forestlevel.R
+++ scripts/test/_dom_archetype_forestlevel.R
@@ -1,3 +1,4 @@
+# This is an important script to include on github. This is a table for 
 library(tidyverse)
 library(terra)
 library(sf)
@@ -35,25 +36,35 @@ fs_reg.proj <- fs_reg %>%
 fs_reg.crop <- st_crop(fs_reg.proj, ext(sgfcm_all_attri_sc))
 
 # read in the 50km buffer shape
-
+# something is wrong with the buffers - they aren't cropped to conus
+# get the intersection with the regional boundaries to "crop" to conus
+# I was getting a bunch of NA for archetypes from water 
 nf_buffers <- read_sf(here::here("data/processed/nf_buffers_50k_2024-10-22.shp"))
 
 # crop the buffers to the outline of conus
 reg_test <- fs_reg.crop %>%
   filter(REGION == "06")
 
+# select a forest to check
 nf_buff_test <- nf_buffers %>%
   filter(FORESTORGC == "0612")
 
+# get the intersection with the region
 test_int <- st_intersection(nf_buff_test, reg_test)
 plot(test_int$geometry)
 
+# create a new nation forest buffers shape
 nf_buffers_int <- st_intersection(nf_buffers, fs_reg.crop)
 
 
 # read in the national forest level shannon diversity shapefile and entropy shapefile 
 shan_nf <- read_sf(here::here("data/processed/shan_h_nf_2024-11-05.shp"))
 ent_nf <- read_sf(here::here("data/processed/ent_nf_2024-11-01.shp"))
+
+# drop the shan div from shan_nf
+shan_nf <- shan_nf %>%
+  select(!shan_dv) %>%
+  select(!shn_dv_)
 
 # calculate the archetype areas assuming crisp archetypes for each forest 
 buff <- nf_buffers_int
@@ -74,7 +85,21 @@ areas_nf <- bind_rows(x, .id = "FORESTORGC") %>%
 areas_nf <- areas_nf %>% 
   replace_na(list(value = 0))
 
-areas <- areas %>%
+nf_arch_areas_for_table <- areas_nf %>%
+  select(FORESTORGC, value, proportion_pct) %>%
+  pivot_wider(names_from = value, values_from = proportion_pct)
+
+# Write to csv
+write_csv(nf_arch_areas_for_table, here::here(paste0("outputs/tables/table_b2_", Sys.Date(), ".csv")))
+
+# recalculate shan diversity and normalized diversity
+shan_h_int <- areas_nf %>%
+  dplyr::select(FORESTORGC, proportion) %>%
+  group_by(FORESTORGC) %>%
+  summarise(shan_div = -sum(proportion * log(proportion)),
+            shan_div_norm = -sum(proportion * log(proportion))/log(6))
+
+areas <- areas_nf %>%
   group_by(FORESTORGC) %>%
   mutate(max_pct = max(proportion_pct)) %>%
   ungroup()
@@ -82,13 +107,17 @@ areas <- areas %>%
 dom_arch_test <- areas %>%
   filter(max_pct == proportion_pct)
 
-dom_arch <- left_join(shan_nf, dom_arch_test, by = c("FORESTO" = "FORESTORGC")) %>%
+dom_arch <- left_join(shan_nf, shan_h_int, by = c("FORESTO" = "FORESTORGC")) %>%
   st_drop_geometry()
-dom_arch <- left_join(ent_nf, dom_arch, by = c("FORESTORGC" = "FORESTO"))
+dom_arch <- left_join(dom_arch, dom_arch_test, by = c("FORESTO" = "FORESTORGC")) %>%
+  st_drop_geometry()
+dom_arch <- left_join(ent_nf, dom_arch, by = c("FORESTORGC" = "FORESTO")) %>%
+  st_drop_geometry()
 
 dom_arch_df <- dom_arch %>%
-  dplyr::select(FORESTNA, FORESTORGC, REGION, value, max_pct, shan_dv, shn_dv_, ent_all, ent_k6_eco, ent_k6_soc) %>%
-  st_drop_geometry() %>%
+  dplyr::select(FORESTNA, FORESTORGC, REGION, value, max_pct, shan_div, shan_div_norm, ent_all, ent_k6_eco, ent_k6_soc) %>%
+  #mutate(shan_div = shan_h_int$shan_div) %>%
+  #mutate(shan_div_norm = shan_h_int$shan_div_norm) %>%
   mutate(ent_eco_sc = scale(ent_k6_eco)[1:109]) %>% 
   mutate(ent_soc_sc = scale(ent_k6_soc)[1:109]) %>%
   mutate(eco_to_soc = case_when(ent_eco_sc < 0 & ent_soc_sc < 0 ~ "low_eco_low_soc",
@@ -98,8 +127,8 @@ dom_arch_df <- dom_arch %>%
   #%>% filter(value != 0)
 names(dom_arch_df) <- c("forest_name", "forest_num", "region", "dom_archetype", "pct_area_dom_arch", "shan_diverse", "shan_diverse_norm", "entropy_all", "entropy_eco", "entropy_soc", "ent_eco_sc", "ent_soc_sc", "eco_to_soc")
 
-#write_csv(dom_arch_df, here::here(paste0("outputs/tables/nf_level_dominant_archetypes_uncertainty_", Sys.Date(), ".csv")))
-dom_arch_df <- read_csv(here::here("outputs/tables/nf_level_dominant_archetypes_uncertainty_2024-11-14.csv"))
+write_csv(dom_arch_df, here::here(paste0("outputs/tables/nf_level_dominant_archetypes_uncertainty_", Sys.Date(), ".csv")))
+#dom_arch_df <- read_csv(here::here("outputs/tables/nf_level_dominant_archetypes_uncertainty_2024-11-14.csv"))
 
 dom_arch_df <- dom_arch_df %>%
   mutate(ent_all_sc = scale(entropy_all)[1:109],
@@ -109,6 +138,17 @@ dom_arch_df <- dom_arch_df %>%
                                 ent_all_sc > 0 & shan_div_sc < 0 ~ "high_ent_low_div",
                                 ent_all_sc > 0 & shan_div_sc > 0 ~ "high_ent_high_div"))
 write_csv(dom_arch_df, here::here(paste0("outputs/tables/nf_level_dominant_archetypes_uncertainty_", Sys.Date(), ".csv")))
+
+table_for_appen <- dom_arch_df %>%
+  select(!entropy_eco) %>%
+  select(!entropy_soc) %>%
+  select(!ent_eco_sc) %>%
+  select(!ent_soc_sc) %>%
+  select(!eco_to_soc) %>%
+  select(!dom_archetype)
+
+write_csv(table_for_appen, here::here(paste0("outputs/tables/table_b3_", Sys.Date(), ".csv")))
+
 
 test_scatter <- dom_arch_df %>%
   ggplot(aes(x=ent_soc_sc, y=ent_eco_sc, group=as.factor(dom_archetype), color=as.factor(dom_archetype))) +

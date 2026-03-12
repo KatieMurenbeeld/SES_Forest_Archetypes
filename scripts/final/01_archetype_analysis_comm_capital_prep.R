f0e9aba2c640c4579efa8fab7b0219349b3638b3
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2026-03-12 16:02:22 -0600

    Updated comments and cleaned up code

--- scripts/final/01_archetype_analysis_comm_capital_prep.R
+++ scripts/final/01_archetype_analysis_comm_capital_prep.R
@@ -1,3 +1,24 @@
+################################################################################
+# SCRIPT TO DOWNLOAD AND PROCESS BASELINE RESILIENCE INDICATORS FOR           ##
+# COMMUNITIES (BRIC) DATA TO 3KM                                              ##
+# THIS IS COUNTY LEVEL DATA AND REFLECTS THE COMMUNITY CAPITAL SEE:           ##
+# https://sc.edu/study/colleges_schools/artsandsciences/centers_and_institutes/hvri/data_and_resources/bric/
+# FOR MORE INFORMATION                                                        ##
+# 1. Load the data                                                            ##
+#  1.1 Filter and select variables                                            ##
+#  1.2 This is county level data so need to download county boundaries from   ##
+#      tigris using the year 2020                                             ##
+# 2. Join the aip data to the county geometeries                              ##
+#  2.1 Check that the new geometries are valid and not empty                  ##
+# 3. Fill in the missing data                                                 ##
+#  3.1 Create an empty raster with 3km resolution                             ##
+#  3.2 Use a custom function to interpolate the missing data                  ##
+# 4. Crop and mask the raster prediction to the ref_raster                    ##
+# 5. Save the raster                                                          ##
+################################################################################
+
+# 0. Load the required libraries
+#-------------------------------------------------------------------------------
 library(stringr)
 library(sf)
 library(terra)
@@ -12,13 +33,25 @@ library(stars)
 # Set the projection
 projection <- "epsg:5070"
 
-# Load the data
+# Load the reference raster
+ref_rast <- rast("/Users/katiemurenbeeld/Analysis/SES_Forest_Archetypes/data/processed/variables/conus_whp_3km_agg_interp_crop_2024-09-27.tif")
+
+# Read in the custom functions
+source(here::here("scripts/functions/custom_functions.R"))
+
+# 1. Load the data
+#-------------------------------------------------------------------------------
+# https://sc.edu/study/colleges_schools/artsandsciences/centers_and_institutes/hvri/documents/bric/bric2020_us_forweb.xlsx
 bric <- read_excel("/Users/katiemurenbeeld/Analysis/Archetype_Analysis/data/original/bric2020_us.xlsx")
 
-# Load the reference raster and reproject
-ref_rast <- rast("/Users/katiemurenbeeld/Analysis/Archetype_Analysis/data/processed/merged/conus_whp_3km_agg_interp_crop_2024-09-27.tif")
-ref_rast_proj <- project(ref_rast, projection)
+## 1.1
+## Select variables and rename GEOID to FIPS and COMM CAPITAL to COMM_CAP
+bric_2020 <- bric %>% # needs 2020 counties
+  dplyr::select("GEOID", "COMM CAPITAL") %>%
+  rename("FIPS" = "GEOID", 
+         "COMM_CAP" = "COMM CAPITAL")
 
+## 1.2
 ## Load county boundaries from tigris
 ### Get Continental US list
 us.abbr <- unique(fips_codes$state)[1:51]
@@ -37,26 +70,17 @@ counties_2020 <- counties_2020 %>%
   filter(STATEFP %in% continental.states$FIPS) %>%
   dplyr::select(GEOID, geometry)
 
-## BRIC: select GEOID and community capital
-bric_2020 <- bric %>% # needs 2020 counties
-  dplyr::select("GEOID", "COMM CAPITAL") %>%
-  rename("FIPS" = "GEOID", 
-         "COMM_CAP" = "COMM CAPITAL")
-
-## Make sure all FIPS codes are padded with 0s for a total of 5 characters
-update_fips <- function(data_set) {
-  data_set$FIPS <- as.character(data_set$FIPS)
-  data_set$FIPS <- str_pad(data_set$FIPS, 5, side="left", pad="0")
-  return(data_set)
-}
-
+## Use the update_fips function to pad all FIPS codes in your data with 0s for
+## a total of 5 characters 
 bric_fips <- update_fips(bric_2020)
 
+# 2. Join the data to the county geometries
+#-------------------------------------------------------------------------------
 ## join to the 2020 counties
 bric_county <- left_join(counties_2020, bric_fips,
                       by = c("GEOID" = "FIPS"))
 
-## check for validity, remove empty geometries, and reproject 
+## 2.1 check for validity, remove empty geometries, and reproject 
 if (!all(st_is_valid(bric_county)))
   bric_county <- st_make_valid(bric_county)
 
@@ -66,7 +90,9 @@ bric_county <- bric_county %>%
 bric_proj <- bric_county %>%
   st_transform(projection)
 
-## Create a template raster for the shapefiles
+# 3. Fill in missing data
+#-------------------------------------------------------------------------------
+## 3.1 Create a template raster for the shapefiles
 XMIN <- ext(ref_rast_proj)$xmin
 XMAX <- ext(ref_rast_proj)$xmax
 YMIN <- ext(ref_rast_proj)$ymin
@@ -81,23 +107,18 @@ templateRas <- rast(ncol=NCOLS, nrow=NROWS,
 
 grd <- st_as_stars(templateRas)
 
-# function to rasterize variable, make points, make predictions
-# raster_grid is like a reference raster
-idw_preds <- function(data_proj, ref_raster, lay, empty_grid){
-  var.rst <- rasterize(data_proj, ref_raster, field = lay, fun = "mean")
-  var.pt <- as.points(var.rst) %>%
-    st_as_sf(.)
-  var.pred <- idw(var.pt[[1]]~1, var.pt, empty_grid)
-  var.pred.rst <- rasterize(st_as_sf(var.pred), ref_raster, field = "var1.pred")
-  names(var.pred.rst) <- paste0(lay, ".pred")
-  #var.pred.rst.crop <- crop(var.pred.rst, ref_raster, mask = TRUE)
-  #return(var.pred.rst)
-  return(c(orig.rst = var.rst, pred.rst = var.pred.rst))
-}
-
+## 3.2 Use the idw_preds function to rasterize and fill in missing data using 
+##     inverse distance weigthing (idw)
 bric.preds <- idw_preds(bric_proj, templateRas, "COMM_CAP", grd)
-plot(bric.preds$orig.rst)
+
+# 4. Crop to reference raster
+#-------------------------------------------------------------------------------
 bric_crop <- crop(bric.preds$pred.rst, ref_rast_proj, mask = TRUE)
+
+# quick check that it cropped
 plot(bric_crop)
-writeRaster(bric_crop, paste0("/Users/katiemurenbeeld/Analysis/Archetype_Analysis/data/processed/bric_commcap_3km_pred_crop_", 
-                                         Sys.Date(), ".tif"))
+
+# 5. Save the raster
+#-------------------------------------------------------------------------------
+#writeRaster(bric_crop, paste0("/Users/katiemurenbeeld/Analysis/SES_Forest_Archetypes/data/processed/variables/bric_commcap_3km_pred_crop_", 
+#                                         Sys.Date(), ".tif"))

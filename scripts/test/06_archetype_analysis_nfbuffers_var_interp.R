167d1eeef5fec4a614e403ab7586b4718b257b9e
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2026-03-30 16:25:00 -0600

    new scripts to look at the results of the k=27 clusters for the national forest level data

---
+++ scripts/test/06_archetype_analysis_nfbuffers_var_interp.R
@@ -0,0 +1,241 @@
+################################################################################
+# SCRIPT TO GENERATE THE "BAR CODE" PLOTS NATIONAL FOREST LEVEL SGFCM CLUSTERS##                 ##
+# 1. Load the data                                                            ##                                   ##
+# 2. Format the data for creating the bar code plots                          ##
+#  k = 27, m = 1.2, alpha = 0.1, beta = 0.1, window size = 3x3                ##
+#  2.1 Save the model as an RDS                                               ##
+# 3. Extract and plot the raster of the cluster groups                        ##
+#  3.1 Save the cluster groups raster                                         ##
+# NOTE TO SELF: INSTEAD OF OSTROM SES NEED TO RENAME TO VARIABLES TO IAD CLASS##
+################################################################################
+
+# 0. Load the required libraries
+#-------------------------------------------------------------------------------
+library(tidyverse)
+library(terra)
+library(sf)
+library(ggplot2)
+library(patchwork)
+library(geocmeans)
+library(RColorBrewer)
+library(viridis)
+library(ggpattern)
+library(distributional)
+library(ggdist)
+library(ggsci)
+library(tigris)
+library(forcats)
+
+# 1. Load the data
+#-------------------------------------------------------------------------------
+## Load the attribute data (unscaled and scaled)
+nfbuffers_data <- rast(here::here("data/processed/nf_buffers_all_attributes_2025-11-06.tif"))
+nfbuffers_data_sc <- rast(here::here("data/processed/nf_buffers_all_attributes_cropped_then_scaled_2025-11-06.tif"))
+
+## Load the resulting cluster raster
+nfbuffer_sgfcm_result <- rast(here::here("outputs/SGFCM_nf_buffers_k27_2026-03-25.tif"))
+
+plot(nfbuffer_sgfcm_result)
+
+## Load the final nation forest level model
+nfbuffer_sgfcm_result_mod <- readRDS(here::here("outputs/SGFCM_nf_buffers_k27_2026-03-25.rds"))
+
+
+# 2. Format the data for creating the bar code plots (variable interpretation plots)
+#-------------------------------------------------------------------------------
+data <- as.data.frame(nfbuffers_data_sc)
+#data <- as.data.frame(scale(nfbuffers_data))
+data$groups <- nfbuffer_sgfcm_result_mod$Groups
+
+data$groups <- gsub('V', 'A', data$groups)
+
+## create a long form data frame
+long_df <- data %>%
+  pivot_longer(!groups, names_to = "var_name")
+
+## get the mean, standard deviation, and median values
+means_long <- data %>%
+  group_by(groups) %>%
+  summarise(across(where(is.numeric), ~ mean(.x, na.rm = TRUE))) %>% 
+  pivot_longer(!groups, names_to = "var_name", values_to = "mean")
+
+sd_long <- data %>%
+  group_by(groups) %>%
+  summarise(across(where(is.numeric), ~ sd(.x, na.rm = TRUE))) %>% 
+  pivot_longer(!groups, names_to = "var_name", values_to = "sd")
+
+med_long <- data %>%
+  group_by(groups) %>%
+  summarise(across(where(is.numeric), ~ median(.x, na.rm = TRUE))) %>% 
+  pivot_longer(!groups, names_to = "var_name", values_to = "median")
+
+## Combine into one data frame with the aggregate values for each group (cluster)
+df_long <- left_join(means_long, sd_long)
+df_long <- left_join(df_long, med_long)
+
+
+## reorder the variables
+long_reorder <- df_long %>% 
+  mutate(var_name = fct_relevel(var_name, 
+                            "treecov", "forprod", "tempseas", 
+                            "precseas", "rough", "whp", 
+                            "forgain", "distcrit", "distwild",
+                            "pm25", "fedrich", 
+                            "treeage", "pct_forpay", "pct_delmill",
+                            "netmig", "comm_cap", "aip", 
+                            "travtime", "hsbrd", "engbrd", "lesshs"))
+
+long_reorder <- long_reorder %>%
+  mutate(ostrom = case_when(var_name == "treecov" | var_name == "forprod" | var_name == "tempseas" | var_name == "precseas" | var_name == "rough" | var_name == "whp" ~ "resource system",
+                            var_name == "distwild" | var_name == "distcrit" | var_name == "forgain" ~ "resource unit",
+                            var_name == "fedrich" | var_name == "pm25" | var_name == "treeage" | var_name == "pct_forpay" | var_name == "pct_delmill" | var_name == "netmig" | var_name == "comm_cap" | var_name == "aip" | var_name == "travtime" | var_name == "hsbrd" | var_name == "engbrd" | var_name == "lesshs" ~ "users"))
+
+# Create bar plots with the mean, standard deviation, and medians
+var_interp <- ggplot(long_reorder, aes(x = var_name, y = mean, fill = ostrom)) +
+  geom_col() +
+  geom_errorbar(aes(ymin=mean-sd, ymax=mean+sd), width=.2,
+                position=position_dodge(.9)) +
+  #scale_fill_brewer(palette = "Set2") +
+  coord_flip() +
+  facet_wrap(~groups) +
+  theme(text = element_text(size = 20),
+        legend.position = "right", 
+        axis.title.y = element_blank()) 
+
+var_interp
+#ggsave(paste0("~/Analysis/SES_Forest_Archetypes/figures/sgfcm_nfbuffers_var_interp_", Sys.Date(), ".png"), 
+#       plot = var_interp, width = 12, height = 8, dpi = 300) 
+
+var_interp_sd <- ggplot(long_reorder, aes(x = var_name, y = sd, fill = ostrom)) +
+  geom_col() +
+  #scale_fill_brewer(palette = "Set2") +
+  coord_flip() +
+  facet_wrap(~groups) +
+  theme(text = element_text(size = 20),
+        legend.position = "right", 
+        axis.title.y = element_blank()) 
+
+var_interp_sd
+ggsave(paste0("~/Analysis/SES_Forest_Archetypes/figures/sgfcm_nfbuffers_var_interp_sd_", Sys.Date(), ".png"), 
+       plot = var_interp_sd, width = 12, height = 8, dpi = 300) 
+
+var_interp_med <- ggplot(long_reorder, aes(x = var_name, y = median, fill = ostrom)) +
+  geom_col() +
+  geom_errorbar(aes(ymin=median-sd, ymax=median+sd), width=.2,
+                position=position_dodge(.9)) +
+  #scale_fill_brewer(palette = "Set2") +
+  coord_flip() +
+  facet_wrap(~groups) +
+  theme(text = element_text(size = 20),
+        legend.position = "right", 
+        axis.title.y = element_blank()) 
+
+var_interp_med
+ggsave(paste0("~/Analysis/SES_Forest_Archetypes/figures/sgfcm_nfbuffers_var_interp_med_", Sys.Date(), ".png"), 
+       plot = var_interp_med, width = 12, height = 8, dpi = 300)
+
+
+# Create bar plots of the variables where IQR != 0
+check_iqr_overlap <- function(x) {
+  # Calculate the 25th and 75th percentiles
+  lower <- quantile(x, 0.25, na.rm = TRUE)
+  upper <- quantile(x, 0.75, na.rm = TRUE)
+  
+  # Check if the IQR range overlaps 0
+  return(lower <= 0 & upper >= 0)
+}
+
+overlap <- long_df %>% 
+  group_by(groups, var_name) %>% 
+  summarise(overlap = check_iqr_overlap(value), .groups="drop")
+
+
+long_join <- long_df %>% 
+  left_join(overlap) %>% 
+  filter(overlap == FALSE)
+
+# reorder the variables
+long_overlap_reorder <- long_join %>% 
+  mutate(var_name = fct_relevel(var_name, 
+                                "treecov", "forprod", "tempseas", 
+                                "precseas", "rough", "whp", 
+                                "forgain", 
+                                #"distcrit", 
+                                "distwild",
+                                "pm25", "fedrich", 
+                                "treeage", "pct_forpay", "pct_delmill",
+                                "netmig", "comm_cap", "aip",
+                                "travtime", "hsbrd", "engbrd"
+                                #"lesshs"
+                                ))
+
+long_overlap_reorder <- long_overlap_reorder %>% # need to remove dist to critical habitat and less hs
+  mutate(ostrom = case_when(var_name == "treecov" | var_name == "forprod" | var_name == "tempseas" | var_name == "precseas" | var_name == "rough" | var_name == "whp" ~ "resource system",
+                            var_name == "distwild" | var_name == "forgain" ~ "resource unit",
+                            var_name == "fedrich" | var_name == "pm25" | var_name == "treeage" | var_name == "pct_forpay" | var_name == "pct_delmill" | var_name == "netmig" | var_name == "comm_cap" | var_name == "aip" | var_name == "travtime" | var_name == "hsbrd" | var_name == "engbrd" | var_name == "lesshs" ~ "users"))
+
+
+iqr_no_overlap <- ggplot(data=long_overlap_reorder, mapping = aes(x=var_name, y=value, fill=ostrom)) +
+  geom_boxplot(outliers = FALSE, coef=0) +
+  geom_hline(yintercept = 0, linetype=2) +
+  #scale_fill_manual(values=c15)+
+  coord_flip()+
+  theme_bw()+
+  facet_wrap(vars(groups))
+
+iqr_no_overlap
+#ggsave(paste0("~/Analysis/Archetype_Analysis/figures/sgfcm_all_k6_var_interp_iqr_no_overlap_", Sys.Date(), ".png"), 
+#       plot = k6_iqr_no_overlap, width = 12, height = 8, dpi = 300)
+
+# Create bar plots of the standard deviation for the values that do overlap with 0
+long_join_overlap_true <- long_df %>% 
+  left_join(overlap) %>% 
+  filter(overlap == TRUE)
+
+sd_overlap <- long_join_overlap_true %>% 
+  group_by(groups, var_name) %>% 
+  summarise(sd = sd(value), .groups="drop")
+
+# reorder the variables
+sd_overlap_reorder <- sd_overlap %>% 
+  mutate(var_name = fct_relevel(var_name, 
+                                #"treecov", 
+                                "forprod", "tempseas", 
+                                "precseas", "rough", "whp", 
+                                "forgain", 
+                                "distcrit", 
+                                "distwild",
+                                #"pm25", 
+                                "fedrich", 
+                                "treeage", "pct_forpay", 
+                                #"pct_delmill",
+                                "netmig", "comm_cap", "aip", 
+                                "travtime", "hsbrd", "engbrd", "lesshs"))
+
+sd_long_overlap_reorder <- sd_overlap_reorder %>% # need to remove treecover, pm25, and pct_delmill
+  mutate(ostrom = case_when(var_name == "forprod" | var_name == "tempseas" | var_name == "precseas" | var_name == "rough" | var_name == "whp" ~ "resource system",
+                            var_name == "distwild" | var_name == "distcrit" |  var_name == "forgain" ~ "resource unit",
+                            var_name == "fedrich" | var_name == "treeage" | var_name == "pct_forpay" | var_name == "netmig" | var_name == "comm_cap" | var_name == "aip" | var_name == "travtime" | var_name == "hsbrd" | var_name == "engbrd" | var_name == "lesshs" ~ "users"))
+
+
+sd_overlap <- ggplot(sd_long_overlap_reorder, aes(x = var_name, y = sd, fill = ostrom)) +
+  geom_col() +
+  #scale_fill_brewer(palette = "Set2") +
+  coord_flip() +
+  facet_wrap(~groups) +
+  theme(text = element_text(size = 20),
+        legend.position = "right", 
+        axis.title.y = element_blank()) 
+
+sd_overlap
+#ggsave(paste0("~/Analysis/Archetype_Analysis/figures/sgfcm_all_k6_var_interp_sd_overlap_", Sys.Date(), ".png"), 
+#       plot = k6_sd_overlap, width = 12, height = 8, dpi = 300)
+
+
+ggplot(data=long_overlap_reorder, mapping = aes(x=var_name, y=sd, fill=ostrom)) +
+  geom_boxplot(outliers = FALSE, coef=0) +
+  geom_hline(yintercept = 0, linetype=2) +
+  #scale_fill_manual(values=c15)+
+  coord_flip()+
+  theme_bw()+
+  facet_wrap(vars(groups_k6))
---
+++ scripts/test/07_archetype_analysis_nfbuffers_spider_plots.R
@@ -0,0 +1,176 @@
+################################################################################
+# SCRIPT TO GENERATE THE RADIAL PLOTS AND CONDENSING CLUSTERS                 ##
+# 1. Load the data                                                            ##                                   ##
+# 2. 
+################################################################################
+
+
+# 0. Load the required libraries
+#-------------------------------------------------------------------------------
+library(geocmeans)
+library(ggplot2)
+library(ggpubr)
+library(dplyr)
+library(viridis)
+library(spdep)
+library(tmap)
+
+spdep::set.mcOption(FALSE)
+spdep::set.coresOption(1L)
+
+
+# 1. Load data (we need the data and the model result)
+#-------------------------------------------------------------------------------
+## Load the scaled attribute data
+Data_nf <- rast(here::here("data/processed/nf_buffers_all_attributes_cropped_then_scaled_2025-11-06.tif"))
+## the data needs to be a data frame
+Data <- as.data.frame(Data_nf, xy = FALSE)
+
+## Load the final nation forest level model
+SGFCM <- readRDS(here::here("outputs/SGFCM_nf_buffers_k27_2026-03-25.rds"))
+
+# 2. From the SGFCM belongings matrix, define the variables needed for the function
+#-------------------------------------------------------------------------------
+# Optional: can quickly plot all of the spider (radial) plots for each cluster 
+spiderPlots(Data, SGFCM$Belongings)
+
+belongmatrix <- SGFCM$Belongings
+data <- Data
+Groups <- ncol(belongmatrix)
+
+Values <- do.call(rbind, lapply(1:Groups, function(i) {
+  W <- belongmatrix[,i]
+  return(apply(data,2, function(row){return(weighted.mean(row,W))}))
+}))
+
+Mins <- apply(Values, 2, min)
+Maxs <- apply(Values, 2, max)
+
+# 3. Create the function (or call in the function)
+#-------------------------------------------------------------------------------
+# This needs more commenting/documentation
+
+create_poly <- function(group_number) {
+  # This creates the data frame for use in the radarchart code
+  # which requires the min, max, and weighted average score 
+  # for each variable for each cluster/group
+  Scores <- Values[group_number,]
+  names(Scores) <- names(data)
+  datam <- data.frame(rbind(Maxs, Mins, Scores))
+  
+  # code for getting the points for the radarchart
+  n <- length(datam)
+  theta <- seq(90, 450, length = n + 1)* (pi/180)
+  theta <- theta[1:n]
+  xx <- cos(theta)
+  yy <- sin(theta)
+  CGap <- ifelse(FALSE, 0, 1) #geocmeans uses centerzero=FALSE
+  # This creates the guidelines for the radarchart
+  # This is needed to scale the values to the radarchart
+  # because CGap is FALSE we need to set up the segments and some scaling
+  seg <- 4 # usually 4 segments labeled 25%, 50%, 75%, 100%
+  series <- length(datam[[1]])
+  SX <- series-2
+  #df <- datam
+  # sets up the scaling
+  for (i in 3:series) {
+    #print(i)
+    xxs <- xx
+    yys <- yy
+    scale <- CGap/(seg+CGap)+(datam[i,]-datam[2,])/(datam[1,]-datam[2,])*seg/(seg+CGap)
+  }
+  # calculate the x and y for each variable, scaled to the radarchart
+  for (j in 1:n) {
+    xxs[j] <- xx[j]*CGap/(seg+CGap)+xx[j]*(datam[i, j]-datam[2, j])/(datam[1, j]-datam[2, j])*seg/(seg+CGap)
+    yys[j] <- yy[j]*CGap/(seg+CGap)+yy[j]*(datam[i, j]-datam[2, j])/(datam[1, j]-datam[2, j])*seg/(seg+CGap)
+  }
+  
+  # create a matrix of the scaled coordinate points for the resulting polygon
+  ply_mat <- cbind(xxs, yys)
+  # need to close the polygon so add the first rows coords to the bottom
+  ply_mat_closed <- rbind(ply_mat, ply_mat[1,])
+  # create a list object
+  ply_list <- list(ply_mat_closed)
+  
+  # from the polygon list object create sf polygons
+  st_geom <- st_polygon(ply_list)
+  st_sfc_geom <- st_sfc(st_geom)
+  clusters_df <- data.frame(cluster = group_number)
+  sf_polygon <- st_as_sf(clusters_df, geometry = st_sfc_geom)
+  
+  # return the final sf_polygon
+  return(sf_polygon) 
+}
+
+# 4. Use the function to create the sf polygons and calculate the overlapping
+##   areas for the polyons
+#-------------------------------------------------------------------------------
+## Here, create an empty matrix and use a for loop to fill it in the 
+## % area overlap for each combination 
+
+## Create empty matrix with nrow and ncol = to the number of groups or clusters
+my_matrix_all <- matrix(data = NA, nrow = 27, ncol = 27)
+print("Initial Matrix:")
+print(my_matrix_all)
+
+## Use a for loop to fill in the matrix with the pct area covered by the 
+## area of intersection for each polygon pair
+
+for (i in 1:nrow(my_matrix_all)){
+  for (j in 1:ncol(my_matrix_all)) {
+    print(c(i, j))
+    # create the sf polygon
+    poly_tmp <- create_poly(i)
+    # create another sf polygon
+    poly_tmp2 <- create_poly(j) 
+    # calculate the overlap
+    overlap_tmp <- st_intersection(poly_tmp, poly_tmp2) 
+    # calculate the area of the overlap
+    area_tmp <- st_area(overlap_tmp) 
+    # calculate the % area of first polygon covered by the area of intersection
+    pct_covered <- area_tmp / st_area(poly_tmp) * 100 
+    # fill in the matrix with the % area covered
+    my_matrix_all[i,j] <- pct_covered
+  }
+}
+
+print(my_matrix_all)
+
+# 5. Save the matrix as a csv
+#-------------------------------------------------------------------------------
+write.csv(as.data.frame(my_matrix_all),
+            file = here::here("outputs/sgfcm_nfbuffers_spider_plot_overlap_matrix_test.csv"))
+
+# 6. Create a matrix with only the instances where [i,j] & [j,i] are both >75%
+#-------------------------------------------------------------------------------
+# from this matrix I want to find the combinations where
+# both [i,j] and [j,i] are over 75%
+
+## create a boolean mask
+mask <- matrix(data = NA, nrow = 27, ncol = 27)
+
+## decide on the threshold
+t <- 80
+
+for (i in 1:27){
+  for (j in 1:27){
+    print(c(i,j))
+    i_j_j_i_true <- my_matrix_all[i,j] > t & my_matrix_all[j,i] > t
+    mask[i,j] <- i_j_j_i_true
+  }
+}
+
+## apply the mask to the matrix
+masked_matrix <- my_matrix_all
+masked_matrix[!mask] <- 0
+print(masked_matrix)
+
+## save the masked matrix as a csv
+write.csv(as.data.frame(masked_matrix),
+          file = here::here("outputs/sgfcm_nfbuffers_spider_plot_overlap_matrix_test_mask.csv"))
+
+
+group9_ply <- create_poly(9)
+
+plot(group4_ply$geometry)
+plot(group9_ply$geometry, add = TRUE, color = "blue")
